a965041fe19952b142f53e32976add6c27c34ea9
Thomas Duigou <thomas.duigou@inrae.fr>
2025-01-15 16:29:35 +0100

    refactor(utils): additional shared functions

--- src/paper/learning/utils.py
+++ src/paper/learning/utils.py
@@ -3,8 +3,12 @@ from typing import Optional, List, Tuple
 from pathlib import Path
 
 import torch
+import pandas as pd
 from torch import Tensor
 from lightning.pytorch.callbacks.early_stopping import EarlyStopping
+from rdkit import Chem
+from rdkit.Chem import DataStructs
+from rdkit.Chem.AllChem import GetMorganGenerator
 import sentencepiece as spm
 
 from paper.dataset.utils import (
@@ -16,6 +20,155 @@ from paper.dataset.utils import (
 
 # Logging -----------------------------------------------------------------------------------------
 logger = logging.getLogger(__name__)
+
+
+# Cheminformatics ---------------------------------------------------------------------------------
+
+def mol_from_smiles(
+        smiles: str,
+        clear_stereo: bool = False,
+        clear_aam: bool = True,
+        clear_isotope: bool = True,
+        clear_hs: bool = True
+) -> Chem.Mol:
+    """Sanitize a molecule
+
+    Parameters
+    ----------
+    smiles : str
+        Smiles string to sanitize.
+    max_mw : int, optional
+        Maximum molecular weight, by default 500.
+    clear_stereo : bool, optional
+        Clear stereochemistry information, by default True.
+    clear_aam : bool, optional
+        Clear atom atom mapping, by default True.
+    clear_isotope : bool, optional
+        Clear isotope information, by default True.
+    clear_hs : bool, optional
+        Clear hydrogen atoms, by default True.
+
+    Returns
+    -------
+    Chem.Mol
+        Sanitized molecule. If smiles is not valid, returns None.
+    """
+    try:
+        if smiles == "nan" or smiles == "" or pd.isna(smiles):
+            return
+        if "." in smiles:  # Reject molecules
+            return
+        if "*" in smiles:   # Reject generic molecules
+            return
+
+        if clear_stereo:  # Wild but effective
+            smiles = smiles.replace("@", "").replace("/", "").replace("\\", "")
+
+        mol = Chem.MolFromSmiles(smiles)
+        if mol is None:
+            return
+
+        if clear_aam:
+            for atom in mol.GetAtoms():
+                atom.SetAtomMapNum(0)
+
+        if clear_isotope:
+            for atom in mol.GetAtoms():
+                atom.SetIsotope(0)
+
+        if clear_stereo or clear_isotope or clear_hs:
+            # Removing stereochemistry and isotope information might leave
+            # the molecule with explicit hydrogens that does not carry any
+            # useful information. We remove them.
+            mol = Chem.RemoveHs(mol)
+
+        return mol
+
+    except Exception as err:
+        raise err
+
+
+def mol_to_smiles(mol: Chem.Mol) -> str:
+    """Get SMILES string for a molecule.
+
+    Parameters
+    ----------
+    mol : Chem.Mol
+        Molecule to convert to SMILES.
+
+    Returns
+    -------
+    str
+        SMILES string. If mol is None, returns None.
+    """
+    if mol is None:
+        smiles = None
+
+    else:
+        try:
+            smiles = Chem.MolToSmiles(mol)
+
+        except Exception:
+            smiles = None
+
+    return smiles
+
+
+def ecfp_to_string(ecfp: DataStructs, sep="-") -> str:
+    """Convert ECFP to a string.
+
+    Bit are duplicated based on their count.
+
+    Parameters
+    ----------
+    ecfp : rdkit.DataStructs
+        ECFP fingerprint.
+    sep : str, optional
+        Separator for the string, by default "-".
+
+    Returns
+    -------
+    str
+        ECFP as a string.
+    """
+    if ecfp is None:
+        return None
+
+    non_zero = []
+    for ix, count in ecfp.GetNonzeroElements().items():
+        non_zero.extend([ix] * count)
+
+    return sep.join([str(x) for x in non_zero])
+
+
+def mol_to_ecfp(mol: Chem.Mol, radius=2, nbits=2048, include_chirality: bool = True) -> DataStructs:
+    """Get ECFP fingerprint for a molecule.
+
+    Parameters
+    ----------
+    mol : Chem.Mol
+        RDKit molecule.
+    radius : int, optional
+        Radius of the ECFP, by default 2.
+    nbits : int, optional
+        Number of bits for the fingerprint, by default 2048.
+    include_chirality : bool, optional
+        Include stereochemistry information, by default True.
+
+    Returns
+    -------
+    DataStructs
+        ECFP fingerprint.
+    """
+    try:
+        return GetMorganGenerator(
+            radius=radius,
+            fpSize=nbits,
+            includeChirality=include_chirality
+        ).GetCountFingerprint(mol)
+
+    except Exception:
+        return None
 
 
 # Tokens ------------------------------------------------------------------------------------------
